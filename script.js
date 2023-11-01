9ec7f4815aa163468981805e532d679edbba4627
AaqibBashirMir <dazzlingmustakeen@gmail.com>
2023-11-01 16:11:21 +0530

    updated

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 let form=document.querySelector("#form");
 let input=document.querySelector("#input");
 let mt=document.querySelector("#movie_title")
-// let res=fetch(`http://www.omdbapi.com/?t=${input.value}&apikey=7d6c964a`)
 
 
 
 
 function display(data){
-        let{Poster,Actors,Title,Year,Runtime,Released}=data;
-        mt.innerHTML="";
-        let div1=document.createElement("div");
-        div1.classList.add("edit");
-        div1.innerHTML=`<img src="${Poster}">
-        <h6>Title:-${Title}</h6>
-        <h6>Year:-${Year}</h6>
-        <h6>Realesed${Released}</h6>
-        <h6>Runtime:-${Runtime}</h6>
-        <h6>Actors:-${Actors}</h6>
-         `
-        mt.appendChild(div1);
-
-
-
+    if (data.Search) {
+      mt.innerHTML="";
+    data.Search.forEach((movie) => {
+      let { Poster, Title, Year } = movie;
+      let div1 = document.createElement("div");
+      div1.classList.add("edit");
+      div1.innerHTML = `
+        <img src="${Poster}">
+        <h6>Title: ${Title}</h6>
+        <h6>Year: ${Year}</h6>
+      `;
+      mt.appendChild(div1);
+    });
+  } else {
+    mt.innerHTML = "No results found.";
+  }
 }
 
 async function getMovies(){
-    let res=await fetch(`http://www.omdbapi.com/?t=${input.value}&apikey=7d6c964a`)
+    let res=await fetch(`https://www.omdbapi.com/?s=${input.value}&page=2&apikey=7d6c964a`)
     let data= await res.json();
     console.log(data);
     display(data);
@@ -40,10 +40,3 @@ form.addEventListener('submit',(e)=>{
     getMovies();
 })
    
-
-
-
-
-
-
-
